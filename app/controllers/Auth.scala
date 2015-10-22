fc5304ec408a3071ff9c4a2125a51b7f7eb459e3
Michal Sznurawa <michal@sznurawa.pl>
2015-10-22 22:44:08 +0100

    Added log in possibility with flickr oauth

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -7,15 +7,29 @@ import play.api.mvc._
 /**
  *
  */
-class Auth extends Controller with Base
+class Auth extends Controller with Base with Flickr
 {
 
   def login = Action { implicit request =>
-    InternalServerError("Not yet implemented")
+    // Actually it does a request to flickr api, so maybe it should be wrapped in future?
+    oauth.retrieveRequestToken(routes.Auth.logon.absoluteURL) match {
+      case Right(token) => TemporaryRedirect(oauth.redirectUrl(token.token)).withSession("token" -> token.token, "secret" -> token.secret)
+      case Left(_) => InternalServerError("Could not retrieve a request token.")
+    }
   }
 
   def logon = Action { implicit request =>
-    InternalServerError("Not yet implemented")
+
+    val accessToken = for {
+      verifier <- request.getQueryString("oauth_verifier")
+      token <- getRequestToken
+    } yield oauth.retrieveAccessToken(token, verifier)
+    // the same case like above - we are doing here a request to flickr api
+
+    accessToken match {
+      case Some(Right(token)) => TemporaryRedirect(routes.Application.index.absoluteURL).withSession("token" -> token.token, "secret" -> token.secret)
+      case _ => InternalServerError("Could not retrieve an access token")
+    }
   }
 
   def logout = Action { implicit request =>
