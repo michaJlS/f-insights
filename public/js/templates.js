94be5d902045505c4eed93a83e626e7eb0d3aba0
Michal Sznurawa <michal@sznurawa.pl>
2016-05-13 21:55:10 +0100

    Small js code reorganization

--- public/js/templates.js
+++ public/js/templates.js
@@ -11,11 +11,15 @@ this["FlickrAssistantTemplates"]["header"] = Handlebars.template({"compiler":[7,
 },"useData":true});
 
 this["FlickrAssistantTemplates"]["home"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"row\">\n    <div class=\"col-md-6 left-panel\"></div>\n    <div class=\"col-md-6 right-panel\"></div>\n</div>";
+    return "<div class=\"row\">\n    <div class=\"col-md-6 left-panel faved-tags\"></div>\n    <div class=\"col-md-6 right-panel faved-authors\"></div>\n</div>\n";
 },"useData":true});
 
 this["FlickrAssistantTemplates"]["layout"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<header id=\"header\"></header>\n<section id=\"main\" class=\"container\"></section>\n<footer id=\"footer\"></footer>";
+    return "<header id=\"header\"></header>\n<nav id=\"menu\"></nav>\n<section id=\"main\" class=\"container\"></section>\n<footer id=\"footer\"></footer>";
+},"useData":true});
+
+this["FlickrAssistantTemplates"]["nav"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<ul class=\"nav nav-pills\">\n    <li role=\"presentation\" class=\"active\"><a href=\"#\">Home</a></li>\n    <li role=\"presentation\"><a href=\"#\">Profile</a></li>\n    <li role=\"presentation\"><a href=\"#\">Messages</a></li>\n</ul>\n";
 },"useData":true});
 
 this["FlickrAssistantTemplates"]["top-faved-authors"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
@@ -25,9 +29,9 @@ this["FlickrAssistantTemplates"]["top-faved-authors"] = Handlebars.template({"1"
     + alias4(((helper = (helper = helpers.owner || (depth0 != null ? depth0.owner : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"owner","hash":{},"data":data}) : helper)))
     + "/\" target=\"_blank\"><img src=\"http://flickr.com/buddyicons/"
     + alias4(((helper = (helper = helpers.owner || (depth0 != null ? depth0.owner : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"owner","hash":{},"data":data}) : helper)))
-    + ".jpg\"> "
+    + ".jpg\"> <strong>"
     + alias4(((helper = (helper = helpers.owner_name || (depth0 != null ? depth0.owner_name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"owner_name","hash":{},"data":data}) : helper)))
-    + "</a></td>\n            <td>"
+    + "</strong></a></td>\n            <td>"
     + alias4(((helper = (helper = helpers.count || (depth0 != null ? depth0.count : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"count","hash":{},"data":data}) : helper)))
     + "</td>\n        </tr>\n        <tr>\n            <td colspan=\"2\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.topphotos : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
@@ -45,17 +49,17 @@ this["FlickrAssistantTemplates"]["top-faved-authors"] = Handlebars.template({"1"
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<h2>Most faved authors</h2>\n<table class=\"table table-striped\">\n    <thead>\n    <tr>\n        <th>Author</th>\n        <th>Count</th>\n    </tr>\n    </thead>\n    <tbody>\n"
+  return "\n<h2>Faved authors <a href=\"#\" class=\"non-contacts\">Show only non-contacts</a></h2>\n<table class=\"table table-striped\">\n    <thead>\n    <tr>\n        <th>Author</th>\n        <th>Count</th>\n    </tr>\n    </thead>\n    <tbody>\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.owners : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    </tbody>\n</table>";
+    + "    </tbody>\n</table>\n<p><a href=\"#\" class=\"moar\">Show more ...</a></p>";
 },"useData":true});
 
 this["FlickrAssistantTemplates"]["top-faved-tags"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "        <tr>\n            <td>"
+  return "        <tr>\n            <td><strong>"
     + alias4(((helper = (helper = helpers.tag || (depth0 != null ? depth0.tag : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"tag","hash":{},"data":data}) : helper)))
-    + "</td>\n            <td>"
+    + "</strong></td>\n            <td>"
     + alias4(((helper = (helper = helpers.count || (depth0 != null ? depth0.count : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"count","hash":{},"data":data}) : helper)))
     + "</td>\n        </tr>\n        <tr>\n            <td colspan=\"2\">\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.topphotos : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
@@ -73,7 +77,7 @@ this["FlickrAssistantTemplates"]["top-faved-tags"] = Handlebars.template({"1":fu
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<h2>Most faved tags</h2>\n<table class=\"table table-striped\">\n    <thead>\n    <tr>\n        <th>Tag</th>\n        <th>Count</th>\n    </tr>\n    </thead>\n    <tbody>\n"
+  return "<h2>Faved tags</h2>\n<table class=\"table table-striped\">\n    <thead>\n    <tr>\n        <th>Tag</th>\n        <th>Count</th>\n    </tr>\n    </thead>\n    <tbody>\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.tags : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    </tbody>\n</table>";
+    + "    </tbody>\n</table>\n<p><a href=\"#\" class=\"moar\">Show more ...</a></p>";
 },"useData":true});
