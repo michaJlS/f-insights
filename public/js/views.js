2466f05ffbe031ddcbc411bf14f1373c5d03a1cc
michajls <michal@szurawa.pl>
2016-11-19 10:35:20 +0000

    Restyling dashboard

--- public/js/views.js
+++ public/js/views.js
@@ -248,6 +248,9 @@ FlickrAssistant.Views.Header = FlickrAssistant.BaseView.extend({
     userInfo: null,
     contacts: null,
     dashboard: null,
+    events:  {
+        "click .update-dashboard": "updateDashboard"
+    },
     initialize: function () {
         this.userInfo = new FlickrAssistant.Models.UserInfo({"nsid": FlickrAssistant.Context.nsid});
         this.contacts = new FlickrAssistant.Collections.Contact(null, {"nsid": FlickrAssistant.Context.nsid});
@@ -255,6 +258,13 @@ FlickrAssistant.Views.Header = FlickrAssistant.BaseView.extend({
         this.userInfo.fetch();
         this.contacts.fetch();
         this.dashboard.fetchWithFallback();
+    },
+    updateDashboard: function () {
+        var updatedDashboard = new FlickrAssistant.Models.Dashboard({"nsid": FlickrAssistant.Context.nsid})
+        updatedDashboard.createNew({success: function(){
+            location.reload();
+        }});
+        return false;
     },
     serialize: function () {
         return {
